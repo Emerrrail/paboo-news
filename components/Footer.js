fab8fb0af05188844e7d54514fc8b01b1c8ceae8
Emerail Wang <Emerail@Emerrrail-deMacBook-Pro.local>
2022-06-10 14:52:32 +0800

    Footer, Navbar and Layout; home and about page

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="">facebook</a>
+        <a href="">instagram</a>
+        <a href="">twitter</a>
+        <a href="">youtube</a>
+      </div>
+      <div>
+        <p>@2022 All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+}
+ 
+export default Footer;
--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
+const About = () => {
+  return ( 
     <div>
-      <h1>Homepage</h1>
+      <h1>About</h1>
       <p>Sunt exercitation laborum consequat dolor elit adipisicing in eiusmod commodo ea irure cupidatat. Adipisicing id irure eiusmod proident excepteur nulla. Fugiat ad laboris veniam occaecat occaecat. Irure proident proident voluptate incididunt incididunt sit labore id amet exercitation in irure ex anim. Amet nisi voluptate officia dolore. Amet aliquip consectetur ullamco est consectetur nisi officia quis.</p>
       <p>Sunt exercitation laborum consequat dolor elit adipisicing in eiusmod commodo ea irure cupidatat. Adipisicing id irure eiusmod proident excepteur nulla. Fugiat ad laboris veniam occaecat occaecat. Irure proident proident voluptate incididunt incididunt sit labore id amet exercitation in irure ex anim. Amet nisi voluptate officia dolore. Amet aliquip consectetur ullamco est consectetur nisi officia quis.</p>
     </div>
-  )
+   );
 }
+ 
+export default About;
